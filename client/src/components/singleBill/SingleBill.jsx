4e3dbfaed0ffbabf3dc50721cf2a82def73b9ecd
Rithick <86591561+Rithickg@users.noreply.github.com>
2022-11-05 13:57:19 +0530

    updates

--- client/src/components/singleBill/SingleBill.jsx
+++ client/src/components/singleBill/SingleBill.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { useRef } from 'react';
 import axios from 'axios';
+import {useReactToPrint} from 'react-to-print'
 import { useLocation } from 'react-router-dom';
 
 export const SingleBill = () => {
@@ -39,37 +41,46 @@ export const SingleBill = () => {
     console.log("bill.order",bill.order)
     console.log(bill)
     console.log("order",order)
+
+    const componentRef =useRef()
+    const handlePrint =useReactToPrint({
+        content: ()=>componentRef.current,
+        documentTitle: bill._id,
+        onAfterPrint: ()=>alert("Print Success")
+    });
+
   return (
-    <div className='bg-gray-900 w-full h-screen flex flex-col justify-center content-center mt-0 mb-0 mx-auto my-auto'>
-        <h1 className='text-rose-500'>SingleBill</h1>
-        <div className='bg-slate-200 w-5/6 flex flex-col justify-center text-center'>
-            <div>
-                <button onClick={handleEdit}>Edit</button>
-                <button onClick={handleDelete}>Delete</button>
-            </div>
-            <div className='flex '>
-            <div className='border -3 border-black'>
-                <h2>INVOICE</h2>
+    <div className=' bg-gray-900 font-serif w-full h-full flex flex-col justify-center'>
+        <h1 className='text-rose-500 text-center m-2 p-2'>Bill NO:{bill._id}</h1>
+        <div ref={componentRef} className=' bg-slate-200 w-auto flex flex-col sm:m-3 md:m-auto  rounded-lg '>
+            <div className='flex flex-wrap px-2 py-2'>
+            <div className='text-md'>
+                <h2 className='text-4xl text-indigo-900 '>INVOICE</h2>
+                <h3>From:</h3>
                 <h4>GST NO:564RV685FT22</h4>
                 <h4>MOB: 8524569513</h4>
                 <h1>SM TRADERS</h1>
                 <h5>Deals in : Building Construction Materials</h5>
                 <h4>Address : No:40-Indhra street,cholaipallam,chennai-78</h4>
-                <hr />
-                <hr />
             </div>
-            <div className='border -3 border-black'>
+            <div className='flex items-center'>
                 <h4>Date:{bill.date}</h4>
             </div>
             </div>
-            <div className='border -3 border-black'>
+            <div className='flex flex-wrap px-2 py-2 border-t-2 border-dotted border-current'>
+            <div className='text-md'>
+                <h3>To:</h3>
                 <h4>M/s.{bill.name}</h4>
-                <h4>Invoice No: {bill._id}</h4>
+                <h4>Mob Number: {bill.mobilenumber}</h4>
                 <h4>Purchaser GST No:{bill.gstnumber}</h4>
                 <h4>To Address:{bill.address}</h4>
             </div>
-            <div>
-            <table className='bill_tab'>
+            <div className='flex items-center'>
+                <h4>Invoice No:{bill._id}</h4>
+            </div>
+            </div>
+            <div className='flex justify-center py-2 text-center border-t-2 border-current border-dotted'>
+            <table>
                     <thead>
                         <tr>
                             <th>Item</th>
@@ -79,10 +90,10 @@ export const SingleBill = () => {
                     </thead>
                     {order.map((q,i)=>(
                        <tbody key={i}>
-                         <tr >
-                           <td>{q.item}</td>
-                           <td>{q.quantity}</td>
-                           <td>{q.amount}</td>
+                         <tr>
+                           <td className='text-stone-900'>{q.item}</td>
+                           <td className='text-stone-900'>{q.quantity}</td>
+                           <td className='text-stone-900'>{q.amount}</td>
                          </tr>
                        </tbody>
                     ))}
@@ -95,7 +106,7 @@ export const SingleBill = () => {
                      </tfoot>
             </table>
             </div>
-            <div className='border -3 border-black'>
+            <div className='text-center py-2 border-t-2 border-current border-dotted'>
                 <h4>Sub Total :{bill.total}</h4>
                 <h4>GST :45</h4>
                 <h4>Total :{`45 + ${parseInt(bill.total)}`}</h4>
@@ -103,15 +114,20 @@ export const SingleBill = () => {
                 <h4>Total in Words:</h4>
                 <hr />
             </div>
-            <div className='border -3 border-black'>
+            <div className='py-2 text-center border-t-2 border-current border-dotted'>
+                <h4>Signature :</h4>
+                <br />
                 <h3>Terms & Conditions :</h3>
                 <p>1).Our Risk and Responsibility as soon as the goods leave our premises.</p>
                 <p>2).No Compliants will be after 24 hours of delivery.</p>
                 <h2>For SM TRADERS</h2>
-                <br />
-                <h4>Signature</h4>
             </div>
         </div>
+        <div className='flex justify-center'>
+                <button className='bg-rose-500 p-2 m-1 rounded-md' onClick={handleEdit}>Edit Bill</button>
+                <button className='bg-rose-500 p-2 m-1 rounded-md' onClick={handleDelete}>Delete Bill</button>
+                <button className='bg-rose-500 p-2 m-1 rounded-md' onClick={handlePrint}>Print Bill</button>
+            </div>
     </div>
   )
 }
